c5381c081769c9e81edf7a0282c6d69f9c70ca06
nancyagrawal11 <nancyagrawal11062006@gmail.com>
2026-09-06 21:25:48 +0530

    Time: 3 ms (47.36%) | Memory: 44.8 MB (80.70%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,36 +1,32 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
 
-
         int[] temp = new int[Math.min(nums1.length, nums2.length)];
         int k = 0;
 
         for (int i = 0; i < nums1.length; i++) {
 
+            // Check if nums1[i] is already added
+            boolean already = false;
+            for (int j = 0; j < k; j++) {
+                if (temp[j] == nums1[i]) {
+                    already = true;
+                    break;
+                }
+            }
+
+            if (already) continue;
+
+            // Linear search in nums2
             for (int j = 0; j < nums2.length; j++) {
-
                 if (nums1[i] == nums2[j]) {
-
-                    boolean duplicate = false;
-
-                    for (int x = 0; x < k; x++) {
-                        if (temp[x] == nums1[i]) {
-                            duplicate = true;
-                            break;
-                        }
-                    }
-
-                    if (!duplicate) {
-                        temp[k++] = nums1[i];
-                    }
-
+                    temp[k++] = nums1[i];
                     break;
                 }
             }
         }
 
         int[] ans = new int[k];
-
         for (int i = 0; i < k; i++) {
             ans[i] = temp[i];
         }
