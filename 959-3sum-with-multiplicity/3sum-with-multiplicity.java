3205ca39e06ad7ebf5ce58cecd08880fd4af7830
nancyagrawal11 <nancyagrawal11062006@gmail.com>
2026-09-17 21:52:44 +0530

    Time: 1339 ms (5.43%) | Memory: 46.9 MB (13.37%) - LeetSync

--- 959-3sum-with-multiplicity/3sum-with-multiplicity.java
+++ 959-3sum-with-multiplicity/3sum-with-multiplicity.java
@@ -1,46 +1,30 @@
 class Solution {
     public int threeSumMulti(int[] arr, int target) {
 
-         long MOD = 1_000_000_007L;
-
-        long[] count = new long[101];
-
-        for (int num : arr) {
-            count[num]++;
-        }
-
+          long MOD = 1_000_000_007L;
         long ans = 0;
 
-        for (int a = 0; a <= 100; a++) {
-            for (int b = a; b <= 100; b++) {
-                int c = target - a - b;
+        for (int i = 0; i < arr.length; i++) {
 
-                if (c < b || c > 100) {
-                    continue;
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                int required = target - arr[i] - arr[j];
+
+                if (map.containsKey(required)) {
+                    ans += map.get(required);
+                    ans %= MOD;
                 }
 
-                if (a == b && b == c) {
-                    // C(n, 3)
-                    ans += count[a] * (count[a] - 1) * (count[a] - 2) / 6;
-                } 
-                else if (a == b) {
-                    // C(count[a], 2) * count[c]
-                    ans += count[a] * (count[a] - 1) / 2 * count[c];
-                } 
-                else if (b == c) {
-                    // count[a] * C(count[b], 2)
-                    ans += count[a] * count[b] * (count[b] - 1) / 2;
-                } 
-                else {
-                    // count[a] * count[b] * count[c]
-                    ans += count[a] * count[b] * count[c];
-                }
-
-                ans %= MOD;
+                map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
             }
         }
 
-         
         return (int) ans;
+
+
+         
+      
     }
 }
